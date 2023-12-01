fc644e1197684ae21048b3f637d3140563ed8f28
Rifqi <rifqimusyaffa0@gmail.com>
2023-12-01 16:38:36 +0700

    fix

--- lib/helper/session_manager.dart
+++ lib/helper/session_manager.dart
@@ -1,70 +1,3 @@
-<<<<<<< HEAD
-// import 'package:flutter/material.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
-// import 'package:wisata_app/Register/login.dart';
-// import 'package:wisata_app/halaman/home_screen.dart';
-
-// class SessionManager {
-//   static SessionManager? _instance;
-//   static shared_preferences? _preferences;
-
-//   static Future<SessionManager> getInstance() async {
-//     if (_instance == null) {
-//       _instance = SessionManager();
-//     }
-
-//     if (_preferences == null) {
-//       _preferences = await SharedPreferences.getInstance();
-//     }
-
-//     return _instance!;
-//   }
-
-//   Future<void> isLogin(BuildContext context) async {
-//     await getInstance();
-//     bool isLogin = _preferences!.getBool('isLogin') ?? false;
-//     if (isLogin) {
-//       Navigator.pushAndRemoveUntil(
-//         context,
-//         MaterialPageRoute(builder: (context) => HomePage()),
-//         (route) => false,
-//       );
-//     }
-//   }
-
-//   Future<void> checkLoginStatus(BuildContext context) async {
-//     await getInstance();
-//     bool isLogin = _preferences!.getBool('isLogin') ?? false;
-
-//     if (!isLogin) {
-//       Navigator.pushAndRemoveUntil(
-//         context,
-//         MaterialPageRoute(builder: (context) => const LoginPage()),
-//         (route) => false,
-//       );
-//     }
-//   }
-
-//   Future<void> saveUserData(String email) async {
-//     await _preferences!.setBool('isLogin', true);
-//     await _preferences!.setString('email', email);
-//   }
-
-//   String? getEmail() {
-//     return _preferences!.getString('email');
-//   }
-
-//   bool? getIsLogin() {
-//     return _preferences!.getBool('isLogin');
-//   }
-
-//   Future<void> clearUserData() async {
-//     await _preferences!.remove('isLogin');
-//     await _preferences!.remove('email');
-//     await _preferences!.clear();
-//   }
-// }
-=======
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:wisata_app/screen/login.dart';
@@ -130,4 +63,3 @@ class SessionManager {
     await _preferences!.clear();
   }
 }
->>>>>>> 8332ae40505e205f05b12c0ea138b291adffa3f9
